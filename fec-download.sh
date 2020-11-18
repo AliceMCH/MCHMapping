a58333f485717cfd104ab3b4e8296c38b5012d51
StefanoBoi <stefano.boi@ca.infn.it>
2020-11-18 18:25:14 +0100

    Corrected a few bugs

--- fec-download.sh
+++ fec-download.sh
@@ -1,26 +1,24 @@
 if [ "$1" == "" ]; then
-    gdown 'https://drive.google.com/uc?id=1JV92ZT5usQrX8DGR5Y2-Ao2qqZ4X1CqF' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=1X_hh_Pq9-JHnYmCE_tdxUJTP78Z3Ainb' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=15-MpWXsLwLI54t2erccWHbHVhjCpfuTa' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=1yE8WIH8Nf1mVdOwwn6CYGc-4eK6B2EA8' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=10Sg55fsHtCvc_V5fCcOo-wXnlw2rb9-I' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=1CA4mw8QTQjI2KaN55kOmMwzGrW10-Yqt' --proxy http://10.161.58.101:8080 &
-    gdown 'https://drive.google.com/uc?id=1bMbt2PGzsfaEyzol5Qjho_ZHBZLqHJvS' --proxy http://10.161.58.101:8080 &
+    gdown 'https://drive.google.com/uc?id=1RTSecebbWJ7rEsc4PQe3GCfcW9KC_bWm' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=1AQFx_uWw_GwHYdJy5J7kr9GUY6jBRQRI' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=1cdmfrL8ggK9Yau3TI7j8nh8WXkseR2UE' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=12y4Csu2oLBV24mEIhWL36kC6JC_1gtRw' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=1GUz2mKMDKiFs6e-RTQY7WQrS4LjfPagR' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=1oeErW_L8HsPNQ82FnBnnfuXhPn3Ob2so' --proxy http://10.161.58.101:8080
+    gdown 'https://drive.google.com/uc?id=1X_QMmym-9SOwXPVumu9LtnSWrP44vCMY' --proxy http://10.161.58.101:8080
 fi
 
 if [ "$1" == "--no-proxy" ]; then
-    gdown 'https://drive.google.com/uc?id=1JV92ZT5usQrX8DGR5Y2-Ao2qqZ4X1CqF' &
-    gdown 'https://drive.google.com/uc?id=1X_hh_Pq9-JHnYmCE_tdxUJTP78Z3Ainb' &
-    gdown 'https://drive.google.com/uc?id=15-MpWXsLwLI54t2erccWHbHVhjCpfuTa' &
-    gdown 'https://drive.google.com/uc?id=1yE8WIH8Nf1mVdOwwn6CYGc-4eK6B2EA8' &
-    gdown 'https://drive.google.com/uc?id=10Sg55fsHtCvc_V5fCcOo-wXnlw2rb9-I' &
-    gdown 'https://drive.google.com/uc?id=1CA4mw8QTQjI2KaN55kOmMwzGrW10-Yqt' &
-    gdown 'https://drive.google.com/uc?id=1bMbt2PGzsfaEyzol5Qjho_ZHBZLqHJvS' &
+    gdown 'https://drive.google.com/uc?id=1RTSecebbWJ7rEsc4PQe3GCfcW9KC_bWm'
+    gdown 'https://drive.google.com/uc?id=1AQFx_uWw_GwHYdJy5J7kr9GUY6jBRQRI'
+    gdown 'https://drive.google.com/uc?id=1cdmfrL8ggK9Yau3TI7j8nh8WXkseR2UE'
+    gdown 'https://drive.google.com/uc?id=12y4Csu2oLBV24mEIhWL36kC6JC_1gtRw'
+    gdown 'https://drive.google.com/uc?id=1GUz2mKMDKiFs6e-RTQY7WQrS4LjfPagR'
+    gdown 'https://drive.google.com/uc?id=1oeErW_L8HsPNQ82FnBnnfuXhPn3Ob2so'
+    gdown 'https://drive.google.com/uc?id=1X_QMmym-9SOwXPVumu9LtnSWrP44vCMY'
 fi
 
 rm -f fec.map
-
-wait
 
 
 for f in ./fec-*.map
